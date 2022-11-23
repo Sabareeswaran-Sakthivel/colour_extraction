008af3cf326ab929858529d54c238c5e87b85817
Sabareeswaran <sabareeswaran.s@freightify.com>
2022-11-23 12:13:10 +0530

    minor change

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ app.get("/api/pantones", async(req, res) => {
   res.send('Success');
 });
 
-const PORT = 8000;
+const PORT = 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
